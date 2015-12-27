a2b7447cf41e440916755ea0e031bbd22582b3ff
OOPer <nagata@shlab.jp>
2015-12-28 07:30:16 +0900

    Fixed comments

--- Classes/GLTextureAtlasViewController.swift
+++ Classes/GLTextureAtlasViewController.swift
@@ -2,7 +2,7 @@
 //  GLTextureAtlasViewController.swift
 //  GLTextureAtlas
 //
-//  Created by 開発 on 2015/12/27.
+//  Translated by OOPer in cooperation with shlab.jp, on 2015/12/27.
 //
 //
 /*
--- Classes/PVRTexture.swift
+++ Classes/PVRTexture.swift
@@ -2,7 +2,7 @@
 //  PVRTexture.swift
 //  GLTextureAtlas
 //
-//  Created by 開発 on 2015/12/27.
+//  Translated by OOPer in cooperation with shlab.jp, on 2015/12/27.
 //
 //
 /*
